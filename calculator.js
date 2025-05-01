a88df07ebf6dc8a8ce55189cd0522524e1a838aa
defalt11-code <royilao1114@gmail.com>
2025-05-01 21:47:49 +0800

    add the addition, subtraction, division and multiplication  function

---
+++ calculator.js
@@ -0,0 +1,20 @@
+const firstOperand = 1;
+const secondOperand = 1;
+
+function add(firstOperand, secondOperand) {
+  return firstOperand + secondOperand;
+}
+
+function subtract(firstOperand, secondOperand) {
+  return firstOperand - secondOperand;
+}
+
+function division(firstOperand, secondOperand) {
+  return firstOperand / secondOperand;
+}
+
+function multiplication(firstOperand, secondOperand) {
+  return firstOperand * secondOperand;
+}
+
+console.log(add(firstOperand, secondOperand));
