b3da14f6be2451199428f08f3766b31b031a933b
defalt11-code <royilao1114@gmail.com>
2025-05-03 11:01:36 +0800

    add the the operate function

--- calculator.js
+++ calculator.js
@@ -1,44 +1,70 @@
 const buttons = document.querySelector('.buttons-container');
 const display = document.querySelector('.display');
 
-let firstOperand = '';
-let operator = '';
-let secondOperand = '';
+let firstOperand = "";
+let secondOperand = "";
+let operator = "";
 
-buttons.addEventListener("click", (event) => {
-  let target = event.target;
-  let value = target.id;
+
+display.textContent = "0";
+
+buttons.addEventListener('click', (event) => {
+  const target = event.target;
+  const value = target.id;
+  console.log(typeof value);
+  if(target.tagName !== "BUTTON") return;
 
   if(value === "clear") {
     firstOperand = "";
-    operator = "";
     secondOperand = "";
+    operator = "";
     display.textContent = "";
     return;
+  };
+
+                            /* GOOD MORNIG SELF! */
+    /* FINISH THIS CODE TODAY!! MAKE YOUR SELF PROUD!!! PLEASE DO IT! */
+          /* FOR YOUR FAMILY DREAMS AND FOR MY DREAM! PLEASE!! */
+          /*  DON'T FORGET TO REMOVE THE EXPRESSION VARIABLES! */
+                              /* GOOD LUCK! */
+
+  if(!isNaN(value) || value === "."){
+    if(!operator) {
+      firstOperand += value;
+      display.textContent = firstOperand;
+    } else {
+      secondOperand += value;
+      display.textContent = firstOperand + operator + secondOperand;
+    }
+  } else if(["+", "-", "*", "/",].includes(value)) {
+    operator = value;
+    display.textContent = firstOperand + operator;
+  } else if(value === "=") {
+    let result = operate(Number(firstOperand), operator, Number(secondOperand));
+    display.textContent = result;
+    firstOperand = result;
+    secondOperand = "";
   }
 
-  if(target.tagName !== "BUTTON") return;
-  console.log(value);
-  console.log(`first operator: ${firstOperand}`);
-  console.log(`Second operand: ${secondOperand}`);
-  console.log(`Operator: ${operator}`);
+  console.log(`VALUE: ${value}`)
 
-  if(!operator) {
-    firstOperand += value;
-    display.textContent = value;
-  }else {
-    secondOperand += value;
-    display.textContent = value;
-  }
+  console.log(`first operand: ${firstOperand}`);
+  console.log(`operator: ${operator}`);
+  console.log(`second operand: ${secondOperand}`);
+
 });
 
-
-
-
-
 function operate(firstOperand, operator, secondOperand) {
-  
+  switch(operator) {
+    case "+":
+       return firstOperand + secondOperand;
+    case "-":
+       return firstOperand - secondOperand;
+    case "*":
+       return firstOperand * secondOperand;
+    case "/":
+      return secondOperand === 0 ? "ERROR" : firstOperand / secondOperand;
+  }
 }
 
-console.log("something is wrong")
-
+// first < 0 ? return : firsOperan / secondOperand
